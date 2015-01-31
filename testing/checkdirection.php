0b51125b11ec4fb16a3909543d51e1cda910f772
Jiachen Yan <jiacheny2@gmail.com>
2015-02-01 05:30:38 +1100

    retrieved all dirID regards to each lineID

--- testing/checkdirection.php
+++ testing/checkdirection.php
@@ -3,14 +3,24 @@
 	require_once '../API_Function.php';
 	
 	$lineIDs = [943, 1517, 7531, 7464, 7458, 7474, 8088, 7476, 7477];
-	$lineID = 7474;
-	$stopID = 22066;
-	$dirID = 33;
-	$time = "2015-01-31T09:00:00Z";
+	$dirIDs =[];
+	$lineID = 7458;
 	
-	BroadNextDepartures($stopID);
-	SpecificNextDepartures($lineID,$stopID,$dirID,$time);
+	$stops = StopsOnLine($lineID);
+
+	for ($i=0; $i<count($stops); $i++) {
+		$stopID = $stops[$i]['stop_id'];
+		echo $stopID,"<br>";
+		$bnd = BroadNextDepartures($stopID);
+		$bnd = reset($bnd);
+		foreach ($bnd as $key => $value) {
+			$dirID = $value["platform"]["direction"]["direction_id"];
+			if ($value['platform']['direction']['line']['line_id']==$lineID && !in_array($dirID, $dirIDs)) {
+				array_push($dirIDs, $dirID);
+			}
+		}
+	}
 	
-	
+	print_r(array_values($dirIDs));
 	
 ?>
