57fd29af569b141e76c708512b5495cd0c678cf4
Ryan Jing <jingr1986@gmail.com>
2015-02-07 21:33:48 +1100

    update

--- stopsOrder.php
+++ stopsOrder.php
@@ -21,8 +21,8 @@
 
 	<div> 
 		<?php
-			$lineID = 943;
-			$dir = 163;
+			$lineID = 7474;
+			$dir = 21;
 			$stops = StopsOnLine($lineID);
 			$stopsidArray = [];
 			$orderID = 1;
@@ -33,7 +33,7 @@
 			
 			$conn = createConnection ();
 			
-			$test = StoppingPattern(42183, $stops[0]["stop_id"], date('2015-02-02\Z'));									
+			$test = StoppingPattern(33829, $stops[0]["stop_id"], date('2015-02-02\Z'));									
 			$test = reset($test);
 							
 			foreach($test as $key => $value){
@@ -43,10 +43,11 @@
 					array_push($stopsidArray, $value["platform"]["stop"]["stop_id"]);
 					$stopID = $value["platform"]["stop"]["stop_id"];
 					$dirID = $value["platform"]["direction"]["direction_id"];
+					$dirName = $value["platform"]["direction"]["direction_name"];
 				
 				}
 				
-				$sql = "INSERT INTO lineStopsOrder (line_id, dir_id, order_id, stop_id) VALUES ($lineID, $dirID, $orderID, $stopID)";
+				$sql = "INSERT INTO lineStopsOrder (line_id, dir_id, dir_name, order_id, stop_id) VALUES ($lineID, $dirID, '$dirName', $orderID, $stopID)";
 				
 				if (mysqli_query($conn, $sql)) {
 					echo "New record created successfully<br>";
--- testing/checkdirection.php
+++ testing/checkdirection.php
@@ -3,6 +3,8 @@
 	require_once '../API_Function.php';
 	require_once '../database.php';
 	
+	$conn = createConnection ();
+	
 	$lineIDs = [943, 1517, 7531, 7464, 7458, 7474, 8088, 7476, 7477];
 	$allDirections = [];
 	
@@ -11,6 +13,7 @@
 		$dirIDs =[];
 		$lineID = $lineIDs[$j];
 		$stops = StopsOnLine($lineID);
+		$tempdirID = 0;
 		
 		for ($i=0; $i<count($stops); $i++) {
 			$stopID = $stops[$i]['stop_id'];
@@ -21,7 +24,21 @@
 				$dirID = $value["platform"]["direction"]["direction_id"];
 				$dirName = $value["platform"]["direction"]["direction_name"];
 				if ($value['platform']['direction']['line']['line_id']==$lineIDs[$j] && !in_array($dirID, array_keys($dirIDs)) ) {
-					$dirIDs[$dirID] = $dirName;
+					$dirIDs[$dirID] = $dirName;					
+					$sql = "select dir_id from direction where line_id = $lineIDs[$j] and dir_name = '$dirName'";
+					$result = getQueryResult($sql);
+					while($row = mysqli_fetch_assoc($result)){
+						$tempdirID = $row['dir_id'];
+					}
+					echo "<br>dir id is ".$tempdirID."<br>";
+					if($dirID != $tempdirID){
+						$sql = "update direction set dir_id = $dirID where line_id = $lineIDs[$j] and dir_name = '$dirName'";
+						if (mysqli_query($conn, $sql)) {
+							echo "update successfully<br>";
+							} else {
+							echo "Error: " . $sql . "<br>" . mysqli_error($conn). "<br>";
+						}
+					}
 					if (count($dirIDs)==2){
 						$allDirections[$lineIDs[$j]]=$dirIDs;
 						print_r(array_values($dirIDs));
@@ -34,9 +51,8 @@
 	
 	print_r(array_values($allDirections));
 	
-	$conn = createConnection ();
-	
-	foreach($allDirections as $key => $value){
+	//generate direction table
+	/*foreach($allDirections as $key => $value){
 		
 		foreach($value as $key2 => $value2){
 			$lineID = $key;
@@ -51,7 +67,7 @@
 					echo "Error: " . $sql . "<br>" . mysqli_error($conn). "<br>";
 				}
 		}
-	}
+	}*/
 	
 	
 				
